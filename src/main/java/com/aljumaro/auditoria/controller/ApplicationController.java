7ecfe8745a4133495608a7ed89791d6b5755eb49
aljumaro <aljumaro@gmail.com>
2016-06-22 17:33:21 +0200

    Error page

--- src/main/java/com/aljumaro/auditoria/controller/ApplicationController.java
+++ src/main/java/com/aljumaro/auditoria/controller/ApplicationController.java
@@ -1,5 +1,6 @@
 package com.aljumaro.auditoria.controller;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/")
-public class ApplicationController {
+public class ApplicationController implements ErrorController{
+
+    private final static String ERROR_PATH = "/error";
 
     @RequestMapping
     public String basePage(){
         return "index";
+    }
+
+    @RequestMapping(ERROR_PATH)
+    public String errorPage(){ return "error"; }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
     }
 }
